25f61de8081105c235388670d7fb8719f2de1351
Saeed Salehi <saeed.salehi@rtl.nl>
2024-12-15 17:55:27 +0100

    add place holder page

---
+++ src/Pages/Expenses.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PersonalExpenseTrackerSystem.Domain.Entities;
+
+namespace Web.Pages;
+
+public class Expenses : PageModel
+{
+    [BindProperty]
+    public List<Expense> Expenses { get; set; }
+    
+    public void OnGet()
+    {
+        //call the service to get the expenses
+        //in order to do that you need to create a bind property and fill it with the expenses
+        // and use the same property in cshtml file
+    }
+    
+    public void OnPost()
+    {
+        //call the service to add the expense
+    }
+}
